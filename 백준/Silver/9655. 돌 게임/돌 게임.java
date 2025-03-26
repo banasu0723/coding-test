d31263abe84130fee6d89c87e4a4a921ee900969
Seungun <102410774+banasu0723@users.noreply.github.com>
2025-03-26 15:33:45 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,23 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int winner = 0;
-        while(N!=0){
-            if(N>3){
-                N-=3;
-            }else{
-                N--;
-            }
-            winner++;
-        }
-        if(winner%2==0){
+        int turnCount = Integer.parseInt(br.readLine());
+        // 짝수일때 찬영 승, 홀수일때 상근 승
+        if(turnCount %2==0){
             System.out.println("CY");
         }else{
             System.out.println("SK");
