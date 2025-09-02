39c418b5b2ec2dfadf088bb6ef4e13be09b7a8c9
Seungun <102410774+banasu0723@users.noreply.github.com>
2025-09-02 13:45:26 +0900

    [Silver IV] Title: 스택, Time: 160 ms, Memory: 19424 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -4,31 +4,55 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+// 10828 스택
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<Integer>();
-        StringBuilder sb = new StringBuilder();
 
-        for(int i=0; i<N; i++){ //
-            StringTokenizer st = new StringTokenizer(br.readLine()); // 공백 기준으로 문자열을 나눠줌
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 명령의 수
+        Stack<Integer> stack = new Stack<Integer>(); // Stack 객체 생성
+        StringBuilder sb = new StringBuilder(); // 출력 성능 향상
+
+        for(int i=0; i<N; i++){
+
+            // 입력을 받고 공백 기준으로 문자열 나눠주기
+            StringTokenizer st = new StringTokenizer(br.readLine());
             String S = st.nextToken();
 
-            if(S.equals("push")) stack.push(Integer.parseInt(st.nextToken()));
-            else if(S.equals("pop")){
-                if(!stack.empty()) sb.append(stack.pop()).append("\n");
-                else sb.append(-1).append("\n");
-            }else if(S.equals("size")){
-                sb.append(stack.size()).append("\n");
-            }else if(S.equals("empty")){
-                if(!stack.empty()) sb.append(0).append("\n");
-                else sb.append(1).append("\n");
-            }else {
-                if(!stack.empty()) sb.append(stack.peek()).append("\n");
-                else sb.append(-1).append("\n");
+            // 명령 확인
+            switch(S){
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if(stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    }else{
+                        sb.append(stack.pop()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty":
+                    if(stack.isEmpty()){
+                        sb.append(1).append("\n");
+                    }else{
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "top":
+                    if(stack.isEmpty()){
+                        sb.append(-1).append("\n");
+                    }else{
+                        sb.append(stack.peek()).append("\n");
+                    }
+                    break;
             }
         }
+
+        // 출력
         System.out.println(sb);
+        
     }
 }
