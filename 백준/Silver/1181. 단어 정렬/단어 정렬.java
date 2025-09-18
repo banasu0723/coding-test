a0f6a3311ae934cef097bf7c06375857ebe18a33
Seungun <102410774+banasu0723@users.noreply.github.com>
2025-09-18 10:48:23 +0900

    [Silver V] Title: 단어 정렬, Time: 396 ms, Memory: 27964 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,38 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    // 1181 단어정렬
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        List<String> words = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            words.add(br.readLine());
+        }
+
+        List<String> sorted = words.stream()
+                .distinct()
+                .sorted(
+                        Comparator.comparingInt(String::length) //길이
+                                .thenComparing(Comparator.naturalOrder()) // 사전순
+                )
+                .collect(Collectors.toList());
+        
+        for(String word : sorted){
+            sb.append(word).append("\n");
+        }
+
+        System.out.println(sb);
+
+
+    }
+}
