4cddc23efab964c39b97245d204ddae6db063387
Seungun <102410774+banasu0723@users.noreply.github.com>
2025-09-20 22:22:41 +0900

    [Silver V] Title: 소트인사이드, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,25 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+// 1427번 소트인사이드
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        String input = br.readLine();
+        String[] digits = input.split("");
+        Arrays.sort(digits, Collections.reverseOrder());
+
+        for(String d:digits){
+            sb.append(d);
+        }
+        System.out.println(sb);
+
+    }
+}
